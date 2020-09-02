28dbcb9bc46e86efb5db211a71d0ec697f8b6967
Stalinium <34775497+Stalinium@users.noreply.github.com>
2020-09-03 00:09:44 +0200

    Update Mandelbrot.py

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -11,7 +11,8 @@ class ComplexNumber:
     def add(self, cn):
 
         self.real += cn.real
-        self.imaginary += cn.imaginary        
+        self.imaginary += cn.imaginary
+        
 
     def square(self):
 
@@ -21,13 +22,14 @@ class ComplexNumber:
 
         return "(" + str(self.real) + " + " + str(self.imaginary) + "i)"
 
+
 start = (-1.5, 1.5)
 end = (0.5, -1.5)
 detail = 1000
 steps = ((end[0] - start[0]) / detail, (end[1] - start[1]) / detail)
-detail2 = 8
 
 arr = np.zeros((detail, detail))
+
 
 for x in range(arr.shape[0]):
     for y in range(arr.shape[1]):
@@ -35,7 +37,7 @@ for x in range(arr.shape[0]):
         cn = ComplexNumber(0, 0)
         cn2 = ComplexNumber(start[0] + x * steps[0], start[1] + y * steps[1])
 
-        for x2 in range(0, detail2):
+        for x2 in range(0, 16):
 
             cn.add(cn2)
             cn = cn.square()
@@ -43,6 +45,8 @@ for x in range(arr.shape[0]):
             if cn.real * cn.real + cn.imaginary * cn.imaginary > 4:
                 arr[x][y] = 1
                 break
+
+    print(str((x / arr.shape[0]) * 100) + " %")
 
 plt.imshow(arr)
 plt.show()
